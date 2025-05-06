153d98b168287a04e43609325f952b6646fbdcb3
haint <haithanh0809@gmail.com>
2025-05-06 11:17:27 +0000

    Mobile Menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
 
+btn.addEventListener('click', navToggle);
 linkForm.addEventListener('submit', formSubmit);
 
 function validURL(src) {
@@ -15,6 +18,12 @@ function validURL(src) {
     'i'
   )
   return !!pattern.test(src);
+}
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
 }
 
 function formSubmit(e) {
